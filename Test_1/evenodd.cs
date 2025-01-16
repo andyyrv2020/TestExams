5c0c022914a9b712a03e8b33ecba0f2f2a7ddbb8
Андрей Рашев <ar42698937@edu.mon.bg>
2025-01-16 21:14:08 +0200

    Add project files.

---
+++ Test_1/evenodd.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_1
+{
+    internal class evenodd
+    {
+        static void palqlq(string[] args)
+
+        {
+
+            int checkNumber = int.Parse(Console.ReadLine());
+
+
+
+            if (checkNumber % 2 == 0)
+
+            {
+
+                Console.WriteLine("even");
+
+            }
+
+
+
+            else
+
+            {
+
+                Console.WriteLine("odd");
+
+            }
+
+        }
+    }
+}
